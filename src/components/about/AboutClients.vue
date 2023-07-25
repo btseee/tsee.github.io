333256e9bbd9027a4606b14405726b80996d432f
Battseren <bbattseren88@gmail.com>
2023-07-25 16:32:12 +0200

    + VueJS update & change

---
+++ src/components/about/AboutClients.vue
@@ -0,0 +1,73 @@
+<script>
+import AboutClientSingle from './AboutClientSingle.vue';
+
+export default {
+	components: {
+		AboutClientSingle,
+	},
+	data: () => {
+		return {
+			clientsHeading: 'Хамтран ажилласан компаниуд',
+			clients: [
+				{
+					id: 1,
+					title: 'Erxes',
+					img: require('@/assets/images/company/erxes.png'),
+				},
+				{
+					id: 2,
+					title: 'Geru',
+					img: require('@/assets/images/company/geru.png'),
+				},
+				{
+					id: 3,
+					title: 'Mongolian Properties',
+					img: require('@/assets/images/company/mongolian_properties.jpg'),
+				},
+				{
+					id: 4,
+					title: 'Monlogistics',
+					img: require('@/assets/images/company/monlogistics.jpg'),
+				},
+				{
+					id: 5,
+					title: 'SendMN',
+					img: require('@/assets/images/company/Sendmn.jpg'),
+				},
+				{
+					id: 6,
+					title: 'Xacbank',
+					img: require('@/assets/images/company/xacbank.png'),
+				},
+				{
+					id: 7,
+					title: 'Octasys',
+					img: require('@/assets/images/company/octasys.png'),
+				},
+				{
+					id: 7,
+					title: 'Хуримын ордон',
+					img: require('@/assets/images/company/wedding.jpg'),
+				},
+			],
+		};
+	},
+};
+</script>
+
+<template>
+	<div class="mt-10 sm:mt-20">
+		<p
+			class="font-general-medium text-2xl sm:text-3xl text-primary-dark dark:text-primary-light"
+		>
+			{{ clientsHeading }}
+		</p>
+		<div class="grid grid-cols-2 sm:grid-cols-4 mt-10 sm:mt-14 gap-2">
+			<AboutClientSingle
+				v-for="client in clients"
+				:key="client.id"
+				:client="client"
+			/>
+		</div>
+	</div>
+</template>
---
+++ src/components/about/AboutMe.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+	setup() {
+		return {
+			bios: [
+				{
+					id: 1,
+					bio:
+						'Im a graduate in Software Engineer from Mongolian University of Science and Technology with a 3 years of experience in this industry. Im a passionate learner whos always willing to learn and work across technologies and UI designing. Also Im pretty capable in NodeJS, Typescript, Angular, ReactJS, MongoDB, PostgreSQL, Python and mentoring new developers.',
+				},
+			],
+		};
+	},
+};
+</script>
+
+<template>
+	<div class="block sm:flex sm:gap-10 mt-10 sm:mt-20">
+		<!-- About profile image -->
+		<div class="w-full sm:w-1/4 mb-7 sm:mb-0">
+			<img
+				src="@/assets/images/profile.png"
+				class="rounded-xl w-96"
+				alt=""
+			/>
+		</div>
+
+		<!-- About details -->
+		<div class="w-full sm:w-3/4 text-left">
+			<p
+				v-for="bio in bios"
+				:key="bio.id"
+				class="font-general-regular mb-4 text-ternary-dark dark:text-ternary-light text-lg"
+			>
+				{{ bio.bio }}
+			</p>
+		</div>
+	</div>
+</template>
